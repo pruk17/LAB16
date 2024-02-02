3c37c668e5e8702a0d157ec8954eaf231f7e1948
Chaiyapruk Parinyanuntakan <chaiyapruk6494@gmail.com>
2024-02-02 17:02:21 +0700

    add lab16

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,36 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void randData(double data[],int N,int M){
+	for(int i = 0; i < N*M; i++){
+        data[i] = (rand()%101)/100.0;
+    }
+}
+
+void showData(double arr[],int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << arr[i];
+        if((i+1)%M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void findRowSum(const double *arr,double *sum1,int N,int M){
+	for(int i = 0;i < N;i++){
+        sum1[i] = 0;
+        for(int j = 0;j < M;j++){
+          sum1[i] += arr[i*M+j];
+        }
+     }
+}
+
+void findColSum(const double *arr,double *sum2,int N,int M){
+     for(int i = 0;i < M;i++){
+        sum2[i] = 0;
+        for(int j = 0;j < N;j++){
+          sum2[i] += arr[j*M+i];
+        }
+     }
+}
